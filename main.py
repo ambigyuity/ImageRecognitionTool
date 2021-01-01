224984984d3636a4561153f3a0fe624c52654f1f
ambigyuity <52841123+ambigyuity@users.noreply.github.com>
2020-12-31 21:26:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -7,14 +6,16 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-#TF_VERSION= tf.version.VERSION
-#print(TF_VERSION)
+
+# TF_VERSION= tf.version.VERSION
+# print(TF_VERSION)
 
 class myCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self,epoch, logs={}):
-        if(logs.get('accuracy')> 0.95):
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('accuracy') > 0.95):
             print("\n Reached 95% accuracy! Discontinue training!")
-            self.model.stop_training=True
+            self.model.stop_training = True
+
 
 def cameraInput(model):
     cv2.namedWindow("SAMSAN TECH")
@@ -30,44 +31,42 @@ def cameraInput(model):
     while rval:
         # frame is the image
         cv2.imshow("SAMSAN TECH", frame)
-        #TODO: MODEL.PREDICT(IMAGE) predict image shown
-        
+        # TODO: MODEL.PREDICT(IMAGE) predict image shown
+
         im = Image.fromarray(frame, 'RGB')
-        im = im.resize((32,32))
+        im = im.resize((32, 32))
         img_array = np.array(im)
-        img_array = np.expand_dims(img_array, axis = 0)
-        
-        #Change labelText to predictionArray or something
-        labelText = model.predict(img_array)[0] 
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Change labelText to predictionArray or something
+        labelText = model.predict(img_array)[0]
         predictiontext = ""
-        
-        #Verify class assignments. Currently it is set alphabetically
+
+        # Verify class assignments. Currently it is set alphabetically
         if labelText[0] == 1:
             predictiontext = "airplane"
-        elif labelText[1] == 1: 
+        elif labelText[1] == 1:
             predictiontext = "automobile"
-        elif labelText[2] == 1: 
+        elif labelText[2] == 1:
             predictiontext = "bird"
-        elif labelText[3] == 1: 
-            predictiontext = "cat"     
-        elif labelText[4] == 1: 
+        elif labelText[3] == 1:
+            predictiontext = "cat"
+        elif labelText[4] == 1:
             predictiontext = "deer"
-        elif labelText[5] == 1: 
+        elif labelText[5] == 1:
             predictiontext = "dog"
-        elif labelText[6] == 1: 
+        elif labelText[6] == 1:
             predictiontext = "frog"
-        elif labelText[7] == 1: 
-            predictiontext = "horse" 
-        elif labelText[8] == 1: 
+        elif labelText[7] == 1:
+            predictiontext = "horse"
+        elif labelText[8] == 1:
             predictiontext = "ship"
-        elif labelText[9] == 1: 
+        elif labelText[9] == 1:
             predictiontext = "truck"
         else:
             predictiontext = None
 
-        
-
-        #labelText= 'Prediction'
+        # labelText= 'Prediction'
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(frame,
                     predictiontext,
@@ -86,21 +85,30 @@ def cameraInput(model):
     cv2.destroyWindow("preview")
     model.summary()
 
-def main():
-    callbacks= myCallback()
-    cifar10= keras.datasets.cifar10
-    (train_images, train_labels), (test_images,test_labels)= cifar10.load_data()
 
-    train_images= train_images.reshape(50000,32,32,3)
-    test_images= test_images.reshape(10000,32,32,3)
-    train_images, test_images= train_images/255.0, test_images/255.0
+def main():
+    callbacks = myCallback()
+    cifar10 = keras.datasets.cifar10
+    (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+
+    train_images = train_images.reshape(50000, 32, 32, 3)
+    test_images = test_images.reshape(10000, 32, 32, 3)
+    train_images, test_images = train_images / 255.0, test_images / 255.0
 
     model = keras.Sequential(
         [
-            #keras.layers.Conv2d(FILTERS, FILTER DIMENSION(3,3), ACTIVATION, INPUTSHAPE)
-            #keras.layers.MaxPooling2D(POOLSIZEX, POOLSIZEY)
-            keras.layers.Conv2D(64,(3,3), activation='relu', input_shape=(32,32,3)),
-            keras.layers.MaxPooling2D(2,2),
+            # keras.layers.Conv2d(FILTERS, FILTER DIMENSION(3,3), ACTIVATION, INPUTSHAPE)
+            # keras.layers.MaxPooling2D(POOLSIZEX, POOLSIZEY)
+            keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+            keras.layers.MaxPooling2D(2, 2),
+            keras.layers.Dropout(0.2),
+            keras.layers.BatchNormalization(),
+            keras.layers.Conv2D(96, (3, 3), activation='relu'),
+            keras.layers.Dropout(0.2),
+            keras.layers.BatchNormalization(),
+            keras.layers.Conv2D(128, (3, 3), activation='relu'),
+            keras.layers.Dropout(0.2),
+            keras.layers.BatchNormalization(),
             keras.layers.Flatten(),
             keras.layers.Dense(256, activation='relu'),
             keras.layers.Dense(10, activation='softmax')
@@ -113,20 +121,17 @@ def main():
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
+    # data_generator= ImageDataGenerator()
+    # TODO: IMPORT IMAGES FROM ZIP AND ALL THAT JAZZ
 
-    #data_generator= ImageDataGenerator()
-    #TODO: IMPORT IMAGES FROM ZIP AND ALL THAT JAZZ 
-
-    #TODO: DATA AUGMENTATION 
-    history= model.fit(train_images, train_labels, epochs=1, callbacks=[callbacks])
+    # TODO: DATA AUGMENTATION
+    history = model.fit(train_images, train_labels, epochs=20, callbacks=[callbacks])
 
     model.evaluate(test_images, test_labels)
 
     cameraInput(model)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
-
-
 
